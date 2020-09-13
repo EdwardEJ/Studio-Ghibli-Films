0589dae661e9ecf2422c04c942717ef55e0c61bc
A <jesus.e.estrada@hotmail.com>
2020-09-13 02:02:32 -0500

    building with heroku

---
+++ public/precache-manifest.93b75425d85667269aa09750f32ea149.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1740fefa1248161fea10fd0e194635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4240ccd0395197e18ad6",
+    "url": "/static/css/main.4022a6e2.chunk.css"
+  },
+  {
+    "revision": "fd965874ed8174280525",
+    "url": "/static/js/2.6e550a8c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.6e550a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4240ccd0395197e18ad6",
+    "url": "/static/js/main.5cfb4036.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7a2a1c2651c27a492300d7b0ed2eb8f1",
+    "url": "/static/media/Studio_Ghibli_logo.7a2a1c26.svg"
+  },
+  {
+    "revision": "53baceef5c8f46ff98be9911a650dbd2",
+    "url": "/static/media/movie-cover.53baceef.jpg"
+  }
+]);
