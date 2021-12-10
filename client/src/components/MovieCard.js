197e7e9e7c06761ce05f1f9f271b36b5b14cc408
A <jesus.e.estrada@hotmail.com>
2021-12-09 21:04:59 -0500

    update

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import name from '../movie-cover.jpg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import name from '../movie-cover.jpg';
 
-const MovieCard = ({ film: { title, director, producer, release_date, id } }) => {
-  return (
-    <div className='movie-card'>
-      <Link to={`Film/${id}`}>
-        <img src={name} alt='dummy' />
-        <div className='movie-content'>
-          <h2>{title}</h2>
-          <p>Director: {director}</p>
-          <p>Producer: {producer}</p>
-          <p>{release_date}</p>
-        </div>
-      </Link>
-    </div >
-  )
-}
+const MovieCard = ({
+	film: { title, director, producer, release_date, id },
+}) => {
+	return (
+		<div id={title} className='movie-card'>
+			<Link to={`Film/${id}`}>
+				<img src={name} alt='dummy' />
+				<div className='movie-content'>
+					<h2>{title}</h2>
+					<p>Director: {director}</p>
+					<p>Producer: {producer}</p>
+					<p>{release_date}</p>
+				</div>
+			</Link>
+		</div>
+	);
+};
 
-export default MovieCard
+export default MovieCard;
