558222698b204f7c971ba2209e6a32ac28432876
A <jesus.e.estrada@hotmail.com>
2021-12-09 21:03:49 -0500

    update

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 // import People from './People'
 
 const GHIBLI_FILM = gql`
- query Film($id: String!) {
-   Film(id: $id) {
-    title
-    description
-    director
-    producer
-    release_date
-    people
-    }
-  }
-`
+	query Film($id: String!) {
+		Film(id: $id) {
+			title
+			description
+			director
+			producer
+			release_date
+			people
+		}
+	}
+`;
 
 const Film = () => {
-  const { id } = useParams()
+	const { id } = useParams();
 
-  const { loading, error, data } = useQuery(GHIBLI_FILM, { variables: { id } });
-  console.log(data)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error</p>
+	const { loading, error, data } = useQuery(GHIBLI_FILM, { variables: { id } });
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error</p>;
 
-  // let peopleArray = people.split(',https://ghibliapi.herokuapp.com/people/')
-  // let test = peopleArray[0].split('https://ghibliapi.herokuapp.com/people/')
-  // test.splice(0, 1)
-  // peopleArray.splice(0, 1)
-  // let peopleIDs = peopleArray.concat(test)
+	// let peopleArray = people.split(',https://ghibliapi.herokuapp.com/people/')
+	// let test = peopleArray[0].split('https://ghibliapi.herokuapp.com/people/')
+	// test.splice(0, 1)
+	// peopleArray.splice(0, 1)
+	// let peopleIDs = peopleArray.concat(test)
 
-  const { title, description, release_date, director, producer } = data.Film
-  console.log(description)
-  return (
-    <div className='person-container'>
-      <div className='person-content'>
-        <h2>{title}</h2>
-        <p>Released in {release_date} directed by {director} and produced by {producer}</p>
-        <p>Description: {description}</p>
-      </div>
-    </div>
-  )
-}
+	const { title, description, release_date, director, producer } = data.Film;
+	return (
+		<div className='person-container'>
+			<div className='person-content'>
+				<h2>{title}</h2>
+				<p>
+					Released in {release_date} directed by {director} and produced by{' '}
+					{producer}
+				</p>
+				<p>Description: {description}</p>
+			</div>
+		</div>
+	);
+};
 
-export default Film
+export default Film;
--- src/components/GhibliMovies.js
+++ src/components/GhibliMovies.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 const GHIBLI_MOVIES = gql`
- query GhibliMoves {
-   Movies {
-    id
-    title
-    director
-    producer
-    release_date
-   }
- }
-`
+	query GhibliMovies {
+		Movies {
+			id
+			title
+			director
+			producer
+			release_date
+		}
+	}
+`;
 
 const GhibliMovies = () => {
-  const { loading, error, data } = useQuery(GHIBLI_MOVIES);
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error</p>
+	const { loading, error, data } = useQuery(GHIBLI_MOVIES);
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error</p>;
 
-  return (
-    <section className='movies-container'>
-      {data.Movies.map(film => (
-        <MovieCard key={film.id} film={film} />
-      )
-      )}
-    </section>
-  )
-}
+	return (
+		<section className='movies-container'>
+			{data.Movies.map((film) => (
+				<MovieCard key={film.id} film={film} />
+			))}
+		</section>
+	);
+};
 
 export default GhibliMovies;
