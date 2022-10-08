f9b28efa77a781d0414f84df477230fd9e387453
A <jesus.e.estrada@hotmail.com>
2022-10-07 23:12:53 -0400

    updating packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
 import { ApolloProvider } from '@apollo/client';
 import React, { Suspense, lazy } from 'react';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const GhibliMovies = lazy(() => import('./components/GhibliMovies'));
 const Film = lazy(() => import('./components/Film'));
 const Header = lazy(() => import('./components/Header'));
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
+	uri: '/graphql',
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Suspense fallback={<p>Loading...</p>}>
-          <div className='App'>
-            <Route path='/' component={Header} />
-            <Route exact path='/' component={GhibliMovies} />
-            <Route exact path='/Film/:id' component={Film} />
-          </div>
-        </Suspense>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<Suspense fallback={<p>Loading...</p>}>
+					<div className='App'>
+						<Routes>
+							<Route path='/' element={<Header />} />
+							<Route exact path='/' element={<GhibliMovies />} />
+							<Route exact path='/Film/:id' element={<Film />} />
+						</Routes>
+					</div>
+				</Suspense>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import name from '../Studio_Ghibli_logo.svg';
+import GhibliLogo from '../Studio_Ghibli_logo';
 
 const Header = () => {
-  return (
-    <header>
-      <h1>
-        <Link to={'/'}>
-          <img src={name} alt='Studio Ghibli' />
-        </Link>
-      </h1>
-    </header>
-  );
+	return (
+		<header>
+			<h1>
+				<Link to={'/'}>
+					<img src={GhibliLogo} alt='Studio Ghibli' />
+				</Link>
+			</h1>
+		</header>
+	);
 };
 
 export default Header;
